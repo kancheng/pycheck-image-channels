bff0f391b0ebcccff434c3a2588cbbbee10c2e9a
tanxin <zxdfgcv@gmail.com>
2024-09-19 15:51:02 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+from PIL import Image
+img = Image.open('./carvana_img.jpg')
+# Directly output the number of channels of the image
+print("INFO. IMG.: carvana_img.jpg; Channels : ",len(img.split()), "; Details : ", img.split())
+
+img = Image.open('./carvana_mask.gif')
+# Directly output the number of channels of the image
+print("INFO. IMG.: carvana_mask.gif; Channels : ",len(img.split()), "; Details : ", img.split())
+
+img = Image.open('./coco128.jpg')
+# Directly output the number of channels of the image
+print("INFO. IMG.: coco128.jpg; Channels : ",len(img.split()), "; Details : ", img.split())
+
+img = Image.open('./mvtec_ad_img1.png')
+# Directly output the number of channels of the image
+print("INFO. IMG.: mvtec_ad_img1.png; Channels : ",len(img.split()), "; Details : ", img.split())
+
+img = Image.open('./mvtec_ad_img2.png')
+# Directly output the number of channels of the image
+print("INFO. IMG.: mvtec_ad_img2.png; Channels : ",len(img.split()), "; Details : ", img.split())
+
+img = Image.open('./mvtec_ad_mask.png')
+# Directly output the number of channels of the image
+print("INFO. IMG.: mvtec_ad_mask.png; Channels : ",len(img.split()), "; Details : ", img.split())
+
+img = Image.open('./salt_img.png')
+# Directly output the number of channels of the image
+print("INFO. IMG.: salt_img.png; Channels : ",len(img.split()), "; Details : ", img.split())
+
+img = Image.open('./salt_mask.png')
+# Directly output the number of channels of the image
+print("INFO. IMG.: salt_mask.png; Channels : ",len(img.split()), "; Details : ", img.split())
+
+img = Image.open('./brats_img.png')
+# Directly output the number of channels of the image
+print("INFO. IMG.: brats_img.png; Channels : ",len(img.split()), "; Details : ", img.split())
+
+img = Image.open('./brats_mask.png')
+# Directly output the number of channels of the image
+print("INFO. IMG.: brats_mask.png; Channels : ",len(img.split()), "; Details : ", img.split())
